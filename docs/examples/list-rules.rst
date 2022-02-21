0da0da9136c673acc8a79da152065db9efceef30
QuentiumYT <quentin.lienhardt@gmail.com>
2022-02-21 16:41:02 +0100

    Added list rules example

---
+++ docs/examples/list-rules.rst
@@ -0,0 +1,7 @@
+List rules script
+=================
+
+.. literalinclude:: ../../examples/list_rules.py
+    :language: python3
+    :caption: This script shows how to list and extract global information from your rules.
+    :linenos:
---
+++ examples/list_rules.py
@@ -0,0 +1,34 @@
+# If you want to run this script, move it one folder above (next to cf.py)
+
+import os, dotenv
+from datetime import datetime
+
+from cf import Cloudflare
+
+dotenv.load_dotenv(".env")
+
+cf = Cloudflare("my_expressions")
+cf.auth(os.environ.get("EMAIL"), os.environ.get("KEY"))
+
+# Get rules
+
+rules = cf.get_rules("ultraperformance.fr")
+
+print(f"There are {rules['count']} rules available for your account:")
+
+print(" ".join([x["description"] for x in rules["result"]]))
+
+# Same as
+
+rules = cf.rules("ultraperformance.fr")
+
+print(f"There are {len(rules)} domains available for your account:")
+
+print(" ".join([x.description for x in rules]))
+
+
+
+for rule in rules:
+    print(f"{rule['description']} is {'paused' if rule['paused'] else 'active'}")
+    created = datetime.fromisoformat(rule['created_on'].replace("Z", ""))
+    print("Created on " + created.strftime("%Y-%m-%d %H:%M:%S"))
