f21d42e750e850b82229b72f28819d0a0637b104
QuentiumYT <quentin.lienhardt@gmail.com>
2022-06-30 15:24:12 +0200

    Bump version

--- cf_rules/__init__.py
+++ cf_rules/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "cf_rules"
 __description__ = "A Cloudflare wrapper to bulk add / edit your firewall rules using Cloudflare's API."
 __url__ = "https://quentiumyt.github.io/Cloudflare-Firewall-Rules/"
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 __author__ = "Quentin Lienhardt"
 __author_email__ = "pro@quentium.fr"
